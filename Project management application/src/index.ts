919074451e519dc8f0bced3ef7aa22673829b9bb
rydvone <rydvone@gmail.com>
2022-11-08 22:17:07 +0300

    refactor: update mongodb link

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://pma-10:pma-team-10@cluster0.cbydybg.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
